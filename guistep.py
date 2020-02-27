113c596e30aeed291f25883a753b40a36429b892
Rasperry_PI <cparkyn@ymail.com>
2020-02-27 19:21:18 +0000

    colin testing 2 buttons

--- guistep.py
+++ guistep.py
@@ -7,7 +7,7 @@ all_pins=[7,11,13,15,31,33,35,37]
 control_pins = [31,33,35,37]
 #control_pins = [7,11,13,15] #first set of gpio pins
 control_pins2 = [7,11,13,15] #second set of gpio pins
-#toapidsf
+
 esttsd=515.000000 #estimated threesixtydegrees in units of full halfstep_seq runs
 #degreeto=sys.argv[1]
 
@@ -34,11 +34,11 @@ halfstep_seq_cw=[ #clockwise sequence
   [1,0,0,1]
 ]
 
-def resetcb():
-  
+def resetcb(pingroup):
+  print(pingroup)
   GPIO.setmode(GPIO.BOARD)
   
-  for pin in all_pins:
+  for pin in pingroup:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
@@ -57,7 +57,7 @@ def resetcb():
   while checka==False:
     for halfstep in range(8):
       for pin in range(4):
-        GPIO.output(control_pins[pin], halfstep_seq_cw[halfstep][pin])
+        GPIO.output(pingroup[pin], halfstep_seq_cw[halfstep][pin])
       time.sleep(0.001)
     holder.seek(0)
     holder.write(str(tickera))
@@ -70,7 +70,7 @@ def resetcb():
   while checkb==False:
     for halfstep in range(8):
       for pin in range(4):
-        GPIO.output(control_pins[pin], halfstep_seq_ccw[halfstep][pin])
+        GPIO.output(pingroup[pin], halfstep_seq_ccw[halfstep][pin])
       time.sleep(0.001)
     tickerb+=1
     if tickerb>=300:
@@ -82,8 +82,10 @@ def resetcb():
 
 
 top=tk.Tk()
-reset=tk.Button(top,text="Press to Reset",command=resetcb)
-reset.pack()
+start=tk.Button(top,text="Press to Start",command=lambda : resetcb(control_pins))
+stop=tk.Button(top,text="Press to Stop",command=lambda : resetcb(control_pins2))
+start.pack()
+stop.pack()
 top.mainloop()
 
 
